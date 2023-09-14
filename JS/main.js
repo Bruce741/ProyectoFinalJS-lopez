40faf8763134edd44030fc2c690a4987f21d4109
Bruno <lopezbruno12319@gmail.com>
2023-09-14 17:33:52 -0300

    .JSON

--- JS/main.js
+++ JS/main.js
@@ -1,93 +1,41 @@
-const suma = (a, b) => a + b;
-const resta = (a, b) => a - b;
-const iva = (x) => x * 0.21;
-const multiplicacion = (a,b) => a*b; 
-
-// Productos // 
-
-class Producto {
-    constructor (info) {
-        this.nombre = info.nombre; 
-        this.precio = info.precio;
-        this.imagen = info.imagen; 
-        this.cantidad = info.cantidad; 
-        this.class = info.class
-    }
-}
-
-const producto1 = new Producto({
-    imagen: "../media/Laptop.png",
-    nombre: "computadora",
-    precio: 100,
-    cantidad: 1,
-    class: "producto1",
-})
-
-const producto2 = new Producto({
-    imagen: "../media/Television.jpg",
-    nombre: "television",
-    precio: 50,
-    cantidad: 1,
-    class: "producto2"
-})
-
-const producto3 = new Producto({
-    imagen: "../media/celular.jpg",
-    nombre: "celular",
-    precio: 40,
-    cantidad: 1,
-    class: "producto3",
-})
-
-const producto4 = new Producto({
-    imagen: "../media/ps5.jpg",
-    nombre: "consola",
-    precio: 100,
-    cantidad: 1,
-    class: "producto4",
-})
-
-// Array de Productos // 
-
-const productos = []; 
-let carrito = []
-
-productos.push(producto1)
-productos.push(producto2)
-productos.push(producto3)
-productos.push(producto4)
+const productos = [];
+let carrito = [];
 
 // HTML //
-
 let divMain = document.getElementById("main");
 
-productos.forEach((item, index) => {
-    let divProductos = document.createElement("div");
-    divProductos.innerHTML = `
-    <img src="${item.imagen}" alt="${item.nombre}">
-    <p>Nombre: ${item.nombre} </p>
-    $${item.precio}
-    <button class="boton">Agregar al carrito </button>
-    `
-    divProductos.className = `${item.class}` 
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    productos.push(...data);
 
-    divMain.append(divProductos);
+    productos.forEach((item, index) => {
+      let divProductos = document.createElement("div");
+      divProductos.innerHTML = `
+        <img src="${item.imagen}" alt="${item.nombre}">
+        <p>Nombre: ${item.nombre}</p>
+        $${item.precio}
+        <button class="boton">Agregar al carrito</button>
+      `;
+      divProductos.className = `${item.class}`;
 
-    // Botones // 
+      divMain.append(divProductos);
 
-    let botones = divProductos.querySelectorAll(".boton");
+      // Botones //
+      let botones = divProductos.querySelectorAll(".boton");
 
-    botones.forEach((boton) => {
+      botones.forEach((boton) => {
         boton.addEventListener("click", () => {
-            const productoSeleccionado = productos[index];
-            carrito.push(productoSeleccionado);
+          const productoSeleccionado = productos[index];
+          carrito.push(productoSeleccionado);
 
-            localStorage.setItem('carrito', JSON.stringify(carrito))
+          localStorage.setItem('carrito', JSON.stringify(carrito));
 
-            actualizarCarritoHTML();
+          actualizarCarritoHTML();
         });
+      });
     });
-});
+  });
 
 
 // Carrito HTML // 
@@ -151,3 +99,8 @@ botonFinalizarCompra.addEventListener("click", () => {
 
     
 
+// por alguna razon no me funciona el servidor local del .json y por ende no estoy pudiendo traer los datos a la pagina // 
+// me salto este error: 
+// " Access to fetch at 'file:///D:/Programacion/Programacion/PROYECTOS/Pre%20Entregas/JavaScript/ProyectoFinal-Lopez/data.json' from origin 'null' has been blocked by CORS policy: Cross origin requests are only supported for protocol schemes: http, data, isolated-app, chrome-extension, chrome, https, chrome-untrusted.  "
+// en teoria hice todos los procedimientos de forma correcta pero no logro solucionar el error // 
+// busque una solucion, involucrando cambiar aspectos de seguridad de chrome, pero no se si funcione para cualquiera que acceda a la pagina o solo para mi dispositivo // 
